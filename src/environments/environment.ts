5aa9ad78db40f5d4ab00e970b5b6962f42817afc
Renan <127274875+Renan-Esteban@users.noreply.github.com>
2024-08-29 01:00:37 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,7 @@ export const environment = {
   production: false,
 
   firebaseConfig: {
-    apiKey: "AIzaSyBZ4k3XKHh8VSzYHLxx9MekzDQSJQ0ao0g",
-    authDomain: "petshop-67d6c.firebaseapp.com",
-    projectId: "petshop-67d6c",
-    storageBucket: "petshop-67d6c.appspot.com",
-    messagingSenderId: "22774719440",
-    appId: "1:22774719440:web:2d86db857cbca5802f4fd5"
+  
   }
 };
 
